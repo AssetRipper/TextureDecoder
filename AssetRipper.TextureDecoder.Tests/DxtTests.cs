f967a521b0e1f0334ad54828e53a577f38ace114
Jeremy Pritts <jrpritts7@gmail.com>
2022-07-06 08:59:00 -0400

    Dxt tests

---
+++ AssetRipper.TextureDecoder.Tests/DxtTests.cs
@@ -0,0 +1,44 @@
+﻿namespace AssetRipper.TextureDecoder.Tests
+{
+	public sealed class DxtTests
+	{
+		[Test]
+		public void DecompressDXT1Test()
+		{
+			ReadOnlySpan<byte> data = File.ReadAllBytes(PathConstants.DxtTestProjectRootFolder + "test.dxt1");
+			int totalBytesRead = 0;
+			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
+			{
+				int bytesRead = Dxt.DxtDecoder.DecompressDXT1(data.Slice(totalBytesRead), size, size, out _);
+				totalBytesRead += bytesRead;
+			}
+			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+		}
+
+		[Test]
+		public void DecompressDXT3Test()
+		{
+			ReadOnlySpan<byte> data = File.ReadAllBytes(PathConstants.DxtTestProjectRootFolder + "test.dxt3");
+			int totalBytesRead = 0;
+			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
+			{
+				int bytesRead = Dxt.DxtDecoder.DecompressDXT3(data.Slice(totalBytesRead), size, size, out _);
+				totalBytesRead += bytesRead;
+			}
+			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+		}
+
+		[Test]
+		public void DecompressDXT5Test()
+		{
+			ReadOnlySpan<byte> data = File.ReadAllBytes(PathConstants.DxtTestProjectRootFolder + "test.dxt5");
+			int totalBytesRead = 0;
+			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
+			{
+				int bytesRead = Dxt.DxtDecoder.DecompressDXT5(data.Slice(totalBytesRead), size, size, out _);
+				totalBytesRead += bytesRead;
+			}
+			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+		}
+	}
+}
