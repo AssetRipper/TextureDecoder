56a70a2895d3ce50077607ad55f920b1273653a2
Jeremy Pritts <jrpritts7@gmail.com>
2022-07-06 10:15:28 -0400

    add a Pvrtc test

---
+++ AssetRipper.TextureDecoder.Tests/PvrtcTests.cs
@@ -0,0 +1,13 @@
+﻿namespace AssetRipper.TextureDecoder.Tests
+{
+	public sealed class PvrtcTests
+	{
+		[Test]
+		public void DecompressPVRTCTest()
+		{
+			byte[] data = File.ReadAllBytes(PathConstants.PvrtcTestProjectRootFolder + "test.pvrtc4");
+			byte[] outputData = new byte[512 * 512 * 4];
+			Pvrtc.PvrtcDecoder.DecompressPVRTC(data, 512, 512, false, outputData);
+		}
+	}
+}
