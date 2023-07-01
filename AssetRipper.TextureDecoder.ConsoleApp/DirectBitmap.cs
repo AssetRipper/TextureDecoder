2085dbcce4850227b623b8576fded883e299dce3
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-07-01 14:16:50 -0400

    Add methods to DirectBitmap

--- AssetRipper.TextureDecoder.ConsoleApp/DirectBitmap.cs
+++ AssetRipper.TextureDecoder.ConsoleApp/DirectBitmap.cs
@@ -43,6 +43,63 @@ public struct DirectBitmap<TColor, TColorArg>
 		}
 	}
 
+	public void SaveAsBmp(string path)
+	{
+		using Stream stream = File.OpenWrite(path);
+		ImageWriter writer = new();
+		if (typeof(TColor) == typeof(ColorRGBA32))
+		{
+			writer.WriteBmp(Data, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
+		}
+		else if (typeof(TColor) == typeof(ColorRGB24))
+		{
+			writer.WriteBmp(Data, Width, Height, ColorComponents.RedGreenBlue, stream);
+		}
+		else
+		{
+			RgbConverter.Convert<TColor, TColorArg, ColorRGBA32, byte>(Bits, Width, Height, out byte[] output);
+			writer.WriteBmp(output, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
+		}
+	}
+
+	public void SaveAsHdr(string path)
+	{
+		using Stream stream = File.OpenWrite(path);
+		ImageWriter writer = new();
+		if (typeof(TColor) == typeof(ColorRGBA32))
+		{
+			writer.WriteHdr(Data, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
+		}
+		else if (typeof(TColor) == typeof(ColorRGB24))
+		{
+			writer.WriteHdr(Data, Width, Height, ColorComponents.RedGreenBlue, stream);
+		}
+		else
+		{
+			RgbConverter.Convert<TColor, TColorArg, ColorRGBA32, byte>(Bits, Width, Height, out byte[] output);
+			writer.WriteHdr(output, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
+		}
+	}
+
+	public void SaveAsJpg(string path)
+	{
+		using Stream stream = File.OpenWrite(path);
+		ImageWriter writer = new();
+		if (typeof(TColor) == typeof(ColorRGBA32))
+		{
+			writer.WriteJpg(Data, Width, Height, ColorComponents.RedGreenBlueAlpha, stream, default);
+		}
+		else if (typeof(TColor) == typeof(ColorRGB24))
+		{
+			writer.WriteJpg(Data, Width, Height, ColorComponents.RedGreenBlue, stream, default);
+		}
+		else
+		{
+			RgbConverter.Convert<TColor, TColorArg, ColorRGBA32, byte>(Bits, Width, Height, out byte[] output);
+			writer.WriteJpg(output, Width, Height, ColorComponents.RedGreenBlueAlpha, stream, default);
+		}
+	}
+
 	public void SaveAsPng(string path)
 	{
 		using Stream stream = File.OpenWrite(path);
@@ -59,6 +116,25 @@ public struct DirectBitmap<TColor, TColorArg>
 		{
 			RgbConverter.Convert<TColor, TColorArg, ColorRGBA32, byte>(Bits, Width, Height, out byte[] output);
 			writer.WritePng(output, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
+		}
+	}
+
+	public void SaveAsTga(string path)
+	{
+		using Stream stream = File.OpenWrite(path);
+		ImageWriter writer = new();
+		if (typeof(TColor) == typeof(ColorRGBA32))
+		{
+			writer.WriteTga(Data, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
+		}
+		else if (typeof(TColor) == typeof(ColorRGB24))
+		{
+			writer.WriteTga(Data, Width, Height, ColorComponents.RedGreenBlue, stream);
+		}
+		else
+		{
+			RgbConverter.Convert<TColor, TColorArg, ColorRGBA32, byte>(Bits, Width, Height, out byte[] output);
+			writer.WriteTga(output, Width, Height, ColorComponents.RedGreenBlueAlpha, stream);
 		}
 	}
 
