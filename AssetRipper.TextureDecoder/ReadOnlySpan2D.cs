5ec29c6a822abc83513b2998ace11d675d53e4bd
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-17 09:46:10 -0500

    Span2D ref structs

---
+++ AssetRipper.TextureDecoder/ReadOnlySpan2D.cs
@@ -0,0 +1,34 @@
+﻿namespace AssetRipper.TextureDecoder;
+
+public readonly ref struct ReadOnlySpan2D<T>
+{
+	public int Width { get; }
+	public int Height { get; }
+	public ReadOnlySpan<T> Data { get; }
+
+	public ReadOnlySpan2D(ReadOnlySpan<T> data, int width, int height)
+	{
+		Validate(data, width, height);
+		Data = data;
+		Width = width;
+		Height = height;
+
+		static void Validate(ReadOnlySpan<T> data, int width, int height)
+		{
+			ArgumentOutOfRangeException.ThrowIfNegative(width);
+			ArgumentOutOfRangeException.ThrowIfNegative(height);
+			if (data.Length != width * height)
+			{
+				throw new ArgumentException("Data length does not match width and height.", nameof(data));
+			}
+		}
+	}
+
+	//public ReadOnlySpan<T> this[int y] => Data.Slice(y * Width, Width);
+
+	public T this[int x, int y] => Data[y * Width + x];
+
+	public static explicit operator ReadOnlySpan2D<T>(T[] array) => new(array, array.Length, 1);
+
+	public static explicit operator ReadOnlySpan2D<T>(ReadOnlySpan<T> span) => new(span, span.Length, 1);
+}
---
+++ AssetRipper.TextureDecoder/Span2D.cs
@@ -0,0 +1,40 @@
+﻿namespace AssetRipper.TextureDecoder;
+
+public readonly ref struct Span2D<T>
+{
+	public int Width { get; }
+	public int Height { get; }
+	public Span<T> Data { get; }
+
+	public Span2D(Span<T> data, int width, int height)
+	{
+		Validate(data, width, height);
+		Data = data;
+		Width = width;
+		Height = height;
+
+		static void Validate(Span<T> data, int width, int height)
+		{
+			ArgumentOutOfRangeException.ThrowIfNegative(width);
+			ArgumentOutOfRangeException.ThrowIfNegative(height);
+			if (data.Length != width * height)
+			{
+				throw new ArgumentException("Data length does not match width and height.", nameof(data));
+			}
+		}
+	}
+
+	//public Span<T> this[int y] => Data.Slice(y * Width, Width);
+
+	public T this[int x, int y]
+	{
+		get => Data[y * Width + x];
+		set => Data[y * Width + x] = value;
+	}
+
+	public static implicit operator ReadOnlySpan2D<T>(Span2D<T> span) => new(span.Data, span.Width, span.Height);
+
+	public static explicit operator Span2D<T>(T[] array) => new(array, array.Length, 1);
+
+	public static explicit operator Span2D<T>(Span<T> span) => new(span, span.Length, 1);
+}
