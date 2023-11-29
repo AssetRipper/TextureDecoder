a25a42dbd4738a39f649c6518fa83783292084ee
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-28 23:34:43 -0500

    Add Etc correctness tests * Resolves #57

--- AssetRipper.TextureDecoder.Tests/EtcTests.cs
+++ AssetRipper.TextureDecoder.Tests/EtcTests.cs
@@ -1,17 +1,19 @@
 using AssetRipper.TextureDecoder.Etc;
+using AssetRipper.TextureDecoder.Rgb;
+using AssetRipper.TextureDecoder.Rgb.Formats;
 
 namespace AssetRipper.TextureDecoder.Tests;
 
 public sealed class EtcTests
 {
-	private static DecodingDelegate ETCDelegate { get; } = (data, width, height) => EtcDecoder.DecompressETC(data, width, height, out _);
-	private static DecodingDelegate ETC2Delegate { get; } = (data, width, height) => EtcDecoder.DecompressETC2(data, width, height, out _);
-	private static DecodingDelegate ETC2A1Delegate { get; } = (data, width, height) => EtcDecoder.DecompressETC2A1(data, width, height, out _);
-	private static DecodingDelegate ETC2A8Delegate { get; } = (data, width, height) => EtcDecoder.DecompressETC2A8(data, width, height, out _);
-	private static DecodingDelegate EACRSignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRSigned(data, width, height, out _);
-	private static DecodingDelegate EACRUnsignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRUnsigned(data, width, height, out _);
-	private static DecodingDelegate EACRGSignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRGSigned(data, width, height, out _);
-	private static DecodingDelegate EACRGUnsignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRGUnsigned(data, width, height, out _);
+	private static DecodingDelegate ETCDelegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressETC(data, width, height, out decompressedData);
+	private static DecodingDelegate ETC2Delegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressETC2(data, width, height, out decompressedData);
+	private static DecodingDelegate ETC2A1Delegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressETC2A1(data, width, height, out decompressedData);
+	private static DecodingDelegate ETC2A8Delegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressETC2A8(data, width, height, out decompressedData);
+	private static DecodingDelegate EACRSignedDelegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressEACRSigned(data, width, height, out decompressedData);
+	private static DecodingDelegate EACRUnsignedDelegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressEACRUnsigned(data, width, height, out decompressedData);
+	private static DecodingDelegate EACRGSignedDelegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressEACRGSigned(data, width, height, out decompressedData);
+	private static DecodingDelegate EACRGUnsignedDelegate { get; } = (ArraySegment<byte> data, int width, int height, out byte[] decompressedData) => EtcDecoder.DecompressEACRGUnsigned(data, width, height, out decompressedData);
 
 	[Test]
 	public void DecompressETCTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc", ETCDelegate);
@@ -37,7 +39,25 @@ public sealed class EtcTests
 	[Test]
 	public void DecompressEACRGUnsignedTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", EACRGUnsignedDelegate);
 
-	private delegate int DecodingDelegate(ArraySegment<byte> data, int width, int height);
+	[Test]
+	public void CorrectnessETCTest() => AssertCorrectDecompression<AndroidTextures.Logo_12, ColorRGB<byte>>(ETCDelegate, .05, 2.1);
+
+	[Test]
+	public void CorrectnessETC2Test() => AssertCorrectDecompression<AndroidTextures.Logo_11, ColorRGB<byte>>(ETC2Delegate, .15, 1.6);
+
+	[Test]
+	public void CorrectnessETC2A1Test() => AssertCorrectDecompression<AndroidTextures.Logo_08>(ETC2A1Delegate, .15, 1.7);
+
+	[Test]
+	public void CorrectnessETC2A8Test() => AssertCorrectDecompression<AndroidTextures.Logo_07>(ETC2A8Delegate, .15, 1.6);
+
+	[Test]
+	public void CorrectnessEACRUnsignedTest() => AssertCorrectDecompression<AndroidTextures.Logo_16, ColorR<byte>>(EACRUnsignedDelegate, .05, .5);
+
+	[Test]
+	public void CorrectnessEACRGUnsignedTest() => AssertCorrectDecompression<AndroidTextures.Logo_15, ColorRG<byte>>(EACRGUnsignedDelegate, .1, .6);
+
+	private delegate int DecodingDelegate(ArraySegment<byte> data, int width, int height, out byte[] decompressedData);
 
 	private static void AssertCorrectByteCountReadFor256SquareWithMips(string path, DecodingDelegate decoder)
 	{
@@ -45,9 +65,38 @@ public sealed class EtcTests
 		int totalBytesRead = 0;
 		foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
 		{
-			int bytesRead = decoder.Invoke(new ArraySegment<byte>(data, totalBytesRead, data.Length - totalBytesRead), size, size);
+			int bytesRead = decoder.Invoke(new ArraySegment<byte>(data, totalBytesRead, data.Length - totalBytesRead), size, size, out _);
 			totalBytesRead += bytesRead;
 		}
 		Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+	}
+
+	private static void AssertCorrectDecompression<T>(DecodingDelegate decoder, double maxMeanDeviation, double maxStandardDeviation) where T : ITexture
+	{
+		AssertCorrectDecompression<T, ColorRGBA<byte>>(decoder, maxMeanDeviation, maxStandardDeviation);
+	}
+
+	private static void AssertCorrectDecompression<TTexture, TColor>(DecodingDelegate decoder, double maxMeanDeviation, double maxStandardDeviation) where TTexture : ITexture where TColor : unmanaged, IColor<TColor, byte>
+	{
+		byte[] data = TTexture.Data;
+		int bytesRead = decoder.Invoke(new ArraySegment<byte>(data), TTexture.Width, TTexture.Height, out byte[] decompressedData);
+		if (!TTexture.Mips)
+		{
+			Assert.That(bytesRead, Is.EqualTo(data.Length));
+		}
+
+		byte[] comparisonData = AndroidTextures.Logo_00.Data;
+
+		//Remove unused color channels
+		if (typeof(TColor) != typeof(ColorBGRA32) && typeof(TColor) != typeof(ColorRGBA<byte>))
+		{
+			Span<ColorBGRA32> pixels = MemoryMarshal.Cast<byte, ColorBGRA32>((Span<byte>)comparisonData);
+			for (int i = 0; i < pixels.Length; i++)
+			{
+				pixels[i] = pixels[i].Convert<ColorBGRA32, byte, TColor, byte>().Convert<TColor, byte, ColorBGRA32, byte>();
+			}
+		}
+
+		ByteArrayDeviation.AssertMinimalDeviation(decompressedData, comparisonData, maxMeanDeviation, maxStandardDeviation);
 	}
 }
