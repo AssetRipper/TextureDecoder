e9674d1c394859618c76c8eced354080e93326a0
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-28 22:59:21 -0500

    Use cached delegates for EtcTests

--- AssetRipper.TextureDecoder.Tests/EtcTests.cs
+++ AssetRipper.TextureDecoder.Tests/EtcTests.cs
@@ -4,79 +4,40 @@ namespace AssetRipper.TextureDecoder.Tests;
 
 public sealed class EtcTests
 {
-	[Test]
-	public void DecompressETCTest()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc", (data, size) =>
-		{
-			return EtcDecoder.DecompressETC(data, size, size, out _);
-		});
-	}
+	private static DecodingDelegate ETCDelegate { get; } = (data, width, height) => EtcDecoder.DecompressETC(data, width, height, out _);
+	private static DecodingDelegate ETC2Delegate { get; } = (data, width, height) => EtcDecoder.DecompressETC2(data, width, height, out _);
+	private static DecodingDelegate ETC2A1Delegate { get; } = (data, width, height) => EtcDecoder.DecompressETC2A1(data, width, height, out _);
+	private static DecodingDelegate ETC2A8Delegate { get; } = (data, width, height) => EtcDecoder.DecompressETC2A8(data, width, height, out _);
+	private static DecodingDelegate EACRSignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRSigned(data, width, height, out _);
+	private static DecodingDelegate EACRUnsignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRUnsigned(data, width, height, out _);
+	private static DecodingDelegate EACRGSignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRGSigned(data, width, height, out _);
+	private static DecodingDelegate EACRGUnsignedDelegate { get; } = (data, width, height) => EtcDecoder.DecompressEACRGUnsigned(data, width, height, out _);
 
 	[Test]
-	public void DecompressETC2Test()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2", (data, size) =>
-		{
-			return EtcDecoder.DecompressETC2(data, size, size, out _);
-		});
-	}
+	public void DecompressETCTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc", ETCDelegate);
 
 	[Test]
-	public void DecompressETC2A1Test()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2a1", (data, size) =>
-		{
-			return EtcDecoder.DecompressETC2A1(data, size, size, out _);
-		});
-	}
+	public void DecompressETC2Test() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2", ETC2Delegate);
 
 	[Test]
-	public void DecompressETC2A8Test()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2a8", (data, size) =>
-		{
-			return EtcDecoder.DecompressETC2A8(data, size, size, out _);
-		});
-	}
+	public void DecompressETC2A1Test() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2a1", ETC2A1Delegate);
 
 	[Test]
-	public void DecompressEACRSignedTest()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_r", (data, size) =>
-		{
-			return EtcDecoder.DecompressEACRSigned(data, size, size, out _);
-		});
-	}
+	public void DecompressETC2A8Test() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2a8", ETC2A8Delegate);
 
 	[Test]
-	public void DecompressEACRUnsignedTest()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_r", (data, size) =>
-		{
-			return EtcDecoder.DecompressEACRUnsigned(data, size, size, out _);
-		});
-	}
+	public void DecompressEACRSignedTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_r", EACRSignedDelegate);
 
 	[Test]
-	public void DecompressEACRGSignedTest()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", (data, size) =>
-		{
-			return EtcDecoder.DecompressEACRGSigned(data, size, size, out _);
-		});
-	}
-	
-	[Test]
-	public void DecompressEACRGUnsignedTest()
-	{
-		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", (data, size) =>
-		{
-			return EtcDecoder.DecompressEACRGUnsigned(data, size, size, out _);
-		});
-	}
+	public void DecompressEACRUnsignedTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_r", EACRUnsignedDelegate);
 
-	private delegate int DecodingDelegate(ArraySegment<byte> data, int size);
+	[Test]
+	public void DecompressEACRGSignedTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", EACRGSignedDelegate);
+
+	[Test]
+	public void DecompressEACRGUnsignedTest() => AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", EACRGUnsignedDelegate);
+
+	private delegate int DecodingDelegate(ArraySegment<byte> data, int width, int height);
 
 	private static void AssertCorrectByteCountReadFor256SquareWithMips(string path, DecodingDelegate decoder)
 	{
@@ -84,7 +45,7 @@ public sealed class EtcTests
 		int totalBytesRead = 0;
 		foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
 		{
-			int bytesRead = decoder.Invoke(new ArraySegment<byte>(data, totalBytesRead, data.Length - totalBytesRead), size);
+			int bytesRead = decoder.Invoke(new ArraySegment<byte>(data, totalBytesRead, data.Length - totalBytesRead), size, size);
 			totalBytesRead += bytesRead;
 		}
 		Assert.That(totalBytesRead, Is.EqualTo(data.Length));
