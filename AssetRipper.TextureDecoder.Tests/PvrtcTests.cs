76a8b2f8370d9a4dd9c895cb1b1a36d6e2bc1550
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-28 22:30:15 -0500

    Update PvrtcTests to assess correctness

--- AssetRipper.TextureDecoder.Tests/PvrtcTests.cs
+++ AssetRipper.TextureDecoder.Tests/PvrtcTests.cs
@@ -1,18 +1,42 @@
-﻿namespace AssetRipper.TextureDecoder.Tests
+﻿using AssetRipper.TextureDecoder.Pvrtc;
+
+namespace AssetRipper.TextureDecoder.Tests;
+
+public sealed class PvrtcTests
 {
-	public sealed class PvrtcTests
+	[Test]
+	public void DecompressPVRTCTest()
 	{
-		[Test]
-		public void DecompressPVRTCTest()
+		ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.PvrtcTestFiles + "test.pvrtc4");
+		int totalBytesRead = 0;
+		foreach (int size in new int[] { 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.PvrtcTestFiles + "test.pvrtc4");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Pvrtc.PvrtcDecoder.DecompressPVRTC(data, size, size, false, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+			int bytesRead = PvrtcDecoder.DecompressPVRTC(data, size, size, false, out _);
+			totalBytesRead += bytesRead;
 		}
+		Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+	}
+
+	private const double MaxMeanDeviation4 = 0.05;
+	private const double MaxStandardDeviation4 = 1.4;
+
+	private const double MaxMeanDeviation2 = 0.10;
+	private const double MaxStandardDeviation2 = 2.6;
+
+	[Test]
+	public void Decompress_4() => AssertCorrectDecompression<AndroidTextures.Logo_13>(false, MaxMeanDeviation4, MaxStandardDeviation4);
+
+	[Test]
+	public void Decompress_2() => AssertCorrectDecompression<AndroidTextures.Logo_14>(true, MaxMeanDeviation2, MaxStandardDeviation2);
+
+	private static void AssertCorrectDecompression<T>(bool do2bitMode, double maxMeanDeviation, double maxStandardDeviation) where T : ITexture
+	{
+		ReadOnlySpan<byte> data = T.Data;
+		int bytesRead = PvrtcDecoder.DecompressPVRTC(data, T.Width, T.Height, do2bitMode, out byte[] decompressedData);
+		if (!T.Mips)
+		{
+			Assert.That(bytesRead, Is.EqualTo(data.Length));
+		}
+		ByteArrayDeviation.AssertMinimalDeviation(decompressedData, AndroidTextures.Logo_00.Data, maxMeanDeviation, maxStandardDeviation);
 	}
 }
