05eaeb81dc935d10d6e36eb18b16410b12af50a8
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-28 22:44:43 -0500

    Refactor EtcTests

--- AssetRipper.TextureDecoder.Tests/EtcTests.cs
+++ AssetRipper.TextureDecoder.Tests/EtcTests.cs
@@ -1,109 +1,92 @@
-namespace AssetRipper.TextureDecoder.Tests
+using AssetRipper.TextureDecoder.Etc;
+
+namespace AssetRipper.TextureDecoder.Tests;
+
+public sealed class EtcTests
 {
-	public sealed class EtcTests
+	[Test]
+	public void DecompressETCTest()
 	{
-		[Test]
-		public void DecompressETCTest()
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.etc");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressETC(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
+			return EtcDecoder.DecompressETC(data, size, size, out _);
+		});
+	}
 
-		[Test]
-		public void DecompressETC2Test()
+	[Test]
+	public void DecompressETC2Test()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.etc2");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressETC2(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
+			return EtcDecoder.DecompressETC2(data, size, size, out _);
+		});
+	}
 
-		[Test]
-		public void DecompressETC2A1Test()
+	[Test]
+	public void DecompressETC2A1Test()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2a1", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.etc2a1");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressETC2A1(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
+			return EtcDecoder.DecompressETC2A1(data, size, size, out _);
+		});
+	}
 
-		[Test]
-		public void DecompressETC2A8Test()
+	[Test]
+	public void DecompressETC2A8Test()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.etc2a8", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.etc2a8");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressETC2A8(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
+			return EtcDecoder.DecompressETC2A8(data, size, size, out _);
+		});
+	}
 
-		[Test]
-		public void DecompressEACRSignedTest()
+	[Test]
+	public void DecompressEACRSignedTest()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_r", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.eac_r");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressEACRSigned(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
+			return EtcDecoder.DecompressEACRSigned(data, size, size, out _);
+		});
+	}
 
-		[Test]
-		public void DecompressEACRUnsignedTest()
+	[Test]
+	public void DecompressEACRUnsignedTest()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_r", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.eac_r");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressEACRUnsigned(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
+			return EtcDecoder.DecompressEACRUnsigned(data, size, size, out _);
+		});
+	}
 
-		[Test]
-		public void DecompressEACRGSignedTest()
+	[Test]
+	public void DecompressEACRGSignedTest()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.eac_rg");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressEACRGSigned(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
-		}
-		
-		[Test]
-		public void DecompressEACRGUnsignedTest()
+			return EtcDecoder.DecompressEACRGSigned(data, size, size, out _);
+		});
+	}
+	
+	[Test]
+	public void DecompressEACRGUnsignedTest()
+	{
+		AssertCorrectByteCountReadFor256SquareWithMips(TestFileFolders.EtcTestFiles + "test.eac_rg", (data, size) =>
 		{
-			ReadOnlySpan<byte> data = File.ReadAllBytes(TestFileFolders.EtcTestFiles + "test.eac_rg");
-			int totalBytesRead = 0;
-			foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
-			{
-				int bytesRead = Etc.EtcDecoder.DecompressEACRGUnsigned(data.Slice(totalBytesRead), size, size, out _);
-				totalBytesRead += bytesRead;
-			}
-			Assert.That(totalBytesRead, Is.EqualTo(data.Length));
+			return EtcDecoder.DecompressEACRGUnsigned(data, size, size, out _);
+		});
+	}
+
+	private delegate int DecodingDelegate(ArraySegment<byte> data, int size);
+
+	private static void AssertCorrectByteCountReadFor256SquareWithMips(string path, DecodingDelegate decoder)
+	{
+		byte[] data = File.ReadAllBytes(path);
+		int totalBytesRead = 0;
+		foreach (int size in new int[] { 256, 128, 64, 32, 16, 8, 4, 2, 1 }) //mip maps
+		{
+			int bytesRead = decoder.Invoke(new ArraySegment<byte>(data, totalBytesRead, data.Length - totalBytesRead), size);
+			totalBytesRead += bytesRead;
 		}
+		Assert.That(totalBytesRead, Is.EqualTo(data.Length));
 	}
 }
