0b4d1dfc8362542b0d6a82169dc34ae87314d8c9
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-01-25 00:12:41 -0800

    Delete benchmarks project

--- AssetRipper.TextureDecoder.Benchmarks/PathConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AssetRipper.TextureDecoder.Benchmarks;
-
-internal static class PathConstants
-{
-	internal const string PathToRepositoryRoot = "../../../../../../../../";
-	internal const string PathToTestFiles = PathToRepositoryRoot + "TestFiles/";
-}
